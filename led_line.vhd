6c794abfb18c6305c12ba52deb6df2d9a6e1fb4b
Andrzej Urbaniak <andrzej.urbaniak@arynga.com>
2017-02-19 17:07:28 +0100

    Added smooth transitions in ledline

--- led_line.vhd
+++ led_line.vhd
@@ -11,18 +11,22 @@ ARCHITECTURE synthesis1 OF led_line IS
 	constant CLOCK_FREQ: INTEGER := 16384;  
 
 	SIGNAL count : unsigned(32 DOWNTO 0);
-	SIGNAL count_pwm : unsigned(6 DOWNTO 0); --128 "ticks" per segment per frame
-	SIGNAL count_segm : unsigned(1 DOWNTO 0); --4 segments
-	SIGNAL count_frame : unsigned(4 DOWNTO 0); --32 frames per second
+	SIGNAL pwm_count : unsigned(6 DOWNTO 0); --128 "ticks" per frame
 	
-	SIGNAL f: std_logic;
-	SIGNAL f_count : unsigned(3 DOWNTO 0);
+	SIGNAL f_count : unsigned(6 DOWNTO 0); --frame counter
+	SIGNAL ledline_clk: std_logic;
+	SIGNAL ledline_count : unsigned(3 DOWNTO 0);
+	SIGNAL leds_val : unsigned(3 DOWNTO 0);
+	SIGNAL leds_pval : unsigned(3 DOWNTO 0);
 BEGIN
-	f <= count(10) WHEN (buttons = "00") ELSE
-	     count(11) WHEN (buttons = "01") ELSE
-		  count(12) WHEN (buttons = "10") ELSE
-		  count(13);
-
+	pwm_count <= count(6 DOWNTO 0);
+	f_count(2 DOWNTO 0) <= "000";
+	f_count(6 DOWNTO 3) <= count(10 DOWNTO 7) WHEN (buttons = "00") ELSE
+	                       count(11 DOWNTO 8) WHEN (buttons = "01") ELSE
+	                       count(12 DOWNTO 9) WHEN (buttons = "10") ELSE
+	                       count(13 DOWNTO 10);
+	ledline_clk <= NOT f_count(6);
+	
    PROCESS (clk)
 	BEGIN
 		IF (clk'EVENT) AND (clk = '1') THEN
@@ -30,23 +34,35 @@ BEGIN
 		END IF;
 	END PROCESS;
 
-	PROCESS (f)
+	PROCESS (ledline_clk)
 	BEGIN
-		IF (f'EVENT AND f = '1') THEN
-			IF (f_count < 5) THEN
-				f_count <= f_count + 1;
+		IF (ledline_clk'EVENT AND ledline_clk = '1') THEN
+			IF (ledline_count < 5) THEN
+				ledline_count <= ledline_count + 1;
 			ELSE
-				f_count <= (others => '0');
+				ledline_count <= (others => '0');
 			END IF;
-		END IF; --(f'EVENT AND f = '1')
+		END IF;
 	END PROCESS;
 
-	WITH f_count SELECT
-		leds <= "1110" WHEN X"0",
-				  "1101" WHEN X"1",
-				  "1011" WHEN X"2",
-				  "0111" WHEN X"3",
-				  "1011" WHEN X"4",
-				  "1101" WHEN X"5",
-				  "1111" WHEN OTHERS;
+	WITH ledline_count SELECT
+		leds_val <= "1110" WHEN X"0",
+				      "1101" WHEN X"1",
+	     			   "1011" WHEN X"2",
+			 	      "0111" WHEN X"3",
+				      "1011" WHEN X"4",
+				      "1101" WHEN X"5",
+				      "1111" WHEN OTHERS;
+
+	WITH ledline_count SELECT
+		leds_pval <= "1101" WHEN X"0",
+				       "1110" WHEN X"1",
+	     		 	    "1101" WHEN X"2",
+			 	       "1011" WHEN X"3",
+				       "0111" WHEN X"4",
+				       "1011" WHEN X"5",
+				       "1111" WHEN OTHERS;
+
+	leds <= std_logic_vector(leds_val) WHEN (f_count > pwm_count) ELSE std_logic_vector(leds_pval);
+
 END synthesis1;
